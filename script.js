41c67721b4ce355ee3cd4a42d94e4f5737af3fc8
calee6 <lee.chelsea727@gmail.com>
2023-04-11 13:00:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,6 @@ if (desktopScreen.matches) {
 
 
 //Mouseover Company: Desktop Version
-/*
 company.addEventListener('mouseover', () => {
     company.style.opacity = '1';
     document.querySelector('.arrow2').style.transform = "scaleY(-1)";
@@ -57,7 +56,29 @@ document.querySelector('.company-dropdown').addEventListener('mouseout', () => {
     document.querySelector('.arrow2').style.transform = "scaleY(1)";
     document.querySelector('.company-dropdown').style.display = 'none';
 });
+
+
+
+
+
+
+
+//Mouseover Company: Mobile Version
+/*
+
+    if (mobileScreen.matches) {
+        //Mouseover Features: Mobile Version
+        features.addEventListener('click', () => {
+            features.classList.toggle('toggle');
+            document.querySelector('.arrow').classList.toggle('toggle');
+            document.querySelector('.features-dropdown').classList.toggle('toggle');
+        });
+
+        //Mouseover Company: Mobile Version
+        company.addEventListener('click', () => {
+            company.classList.toggle('toggle');
+            document.querySelector('.arrow2').classList.toggle('toggle');
+            document.querySelector('.company-dropdown').classList.toggle('toggle');
+        });
+    }
 */
-
-
-
