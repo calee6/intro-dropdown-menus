16d3812d3331cfd1e5afd5ea679af23ca00d314f
calee6 <lee.chelsea727@gmail.com>
2023-04-16 23:07:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,45 +4,31 @@ const hamburger = document.querySelector('.hamburger');
 const bar1 = document.querySelector('.bar1');
 const bar2 = document.querySelector('.bar2');
 const bar3 = document.querySelector('.bar3');
+const mediaQuery = window.matchMedia('(max-width: 650px)');
+const desktopView = window.matchMedia('(min-width: 651px)');
 
-//Events for 'Features' (Desktop)
-document.querySelector('.features').addEventListener('mouseover', () => {
-    featuresDropdown.style.display = 'flex';
-    document.querySelector('.arrow').classList.add('toggle');
+// Events for 'Features' (Hover)
+document.querySelector('.features').addEventListener('click', () => {
+    featuresDropdown.classList.toggle('toggle');
+    document.querySelector('.arrow').classList.toggle('toggle');
 });
-featuresDropdown.addEventListener('mouseover', () => {
-    featuresDropdown.style.display = 'flex';
-    document.querySelector('.arrow').classList.add('toggle');
-});
-document.querySelector('.features').addEventListener('mouseout', () => {
-    featuresDropdown.style.display = 'none';
-    document.querySelector('.arrow').classList.remove('toggle');
 
-});
-featuresDropdown.addEventListener('mouseout', () => {
-    featuresDropdown.style.display = 'none';
+document.querySelector('main').addEventListener('click', () => {
+    featuresDropdown.classList.remove('toggle');
     document.querySelector('.arrow').classList.remove('toggle');
 });
 
 
-//Events for 'Company' (Desktop)
-document.querySelector('.company').addEventListener('mouseover', () => {
-    companyDropdown.style.display = 'flex';
-    document.querySelector('.arrow2').classList.add('toggle');
-});
-companyDropdown.addEventListener('mouseover', () => {
-    companyDropdown.style.display = 'flex';
-    document.querySelector('.arrow2').classList.add('toggle');
-});
-document.querySelector('.company').addEventListener('mouseout', () => {
-    companyDropdown.style.display = 'none';
-    document.querySelector('.arrow2').classList.remove('toggle');
-});
-companyDropdown.addEventListener('mouseout', () => {
-    companyDropdown.style.display = 'none';
-    document.querySelector('.arrow2').classList.remove('toggle');
+// Events for 'Company' (Desktop)
+document.querySelector('.company').addEventListener('click', () => {
+    companyDropdown.classList.toggle('toggle');
+    document.querySelector('.arrow2').classList.toggle('toggle');
 });
 
+document.querySelector('main').addEventListener('click', () => {
+    companyDropdown.classList.remove('toggle');
+    document.querySelector('.arrow2').classList.remove('toggle');
+});
 
 
 
@@ -54,3 +40,32 @@ hamburger.addEventListener('click', () => {
     document.querySelector('.nav-links-container').classList.toggle('toggle');
     document.querySelector('.black-bg').classList.toggle('toggle');
 });
+
+document.querySelector('.black-bg').addEventListener('click', () => {
+    document.querySelector('.nav-links-container').classList.remove('toggle');
+    document.querySelector('.black-bg').classList.toggle('toggle');
+    bar1.classList.toggle('toggle');
+    bar2.classList.toggle('toggle');
+    bar3.classList.toggle('toggle');
+});
+
+
+//Alternative desktopView
+//mediaQuery.addListener(mobileScreen);
+
+mediaQuery.addEventListener('change', mobileScreen);
+
+function mobileScreen(e) {
+        
+    if (e.matches) {
+        document.querySelector('.hero').setAttribute('src', 'images/image-hero-mobile.png');
+
+    }
+    else {
+        document.querySelector('.hero').setAttribute('src', 'images/image-hero-desktop.png');
+
+    }
+
+}
+
+mobileScreen(mediaQuery);
